cfe8081eb7c9ed32291ba5493aff875b9cdf5036
Vitto <admin@vitto.io>
2022-12-25 12:30:45 +0100

    npm preparations

--- scripts/utils/helpers.ts
+++ scripts/utils/helpers.ts
@@ -86,6 +86,7 @@ const getGasPrice = async function (): Promise<GasParams> {
       return {
         gasPrice: null,
         type: 2,
+        //maxPriorityFeePerGas: gasPrice.sub(gasPricing.nextBlockFee!),
         maxPriorityFeePerGas: gasPrice,
         maxFeePerGas: gasPrice,
       };
